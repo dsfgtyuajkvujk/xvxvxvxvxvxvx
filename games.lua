d23ea0cdc682a908c84f1930537a9b814e0e4d1f
dsfgtyuajkvujk <fortune.lua@protonmail.com>
2026-01-03 22:22:42 -0800

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,5 +1,27 @@
 local Games = {
-  [87039211657390] = "https://raw.githubusercontent.com/dsfgtyuajkvujk/xvxvxvxvxvxvx/refs/heads/main/AC.lua", -- Arise Crossover
+  [87039211657390] = "https://raw.githubusercontent.com/dsfgtyuajkvujk/xvxvxvxvxvxvx/refs/heads/main/AC.lua", -- Blox Fruit Sea 1
+  [0] = "", -- Blox Fruit Sea 2
+  [0] = "", -- Blox Fruit Sea 3
+  [0] = "", -- Muscle Legends
+  [0] = "",  -- PS99 World 1
+  [0] = "",  -- PS99 World 2
+  [0] = "",  -- PS99 World 3
+  [0] = "",  -- The Strongest Battlegrounds
+  [0] = "", -- Pet GO
+  [0] = "", -- Fisch
+  [0] = "", -- 2 Sea Fisch
+  [0] = "", -- Dead Rails
+  [0] = "", -- Dead Rails
+  [0] = "", -- Drill Digging Simulator
+  [0] = "", -- Arise Crossover
+  [0] = "", -- Arise Crossover | Dungeon
+  [0] = "", -- Arise Crossover | AFK Reward
+  [0] = "", -- Bubble Gum Simulator INFINITY
+  [0] = "", -- Grow a Garden
+  [0] = "", -- Grow a Garden 1
+  [0] = "", -- Dig
+  [0] = "", -- 99 Nights in the Forest
+  [0] = "", -- 99 Nights in the Forest 1
 }
 
 return Games
